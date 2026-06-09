4f6e79110191d769f00f07ea85ab3113150f9f40
kyomega85 <kyomega85@gmail.com>
2026-06-09 15:41:48 -0600

    test5

--- obj/Debug/net10.0/EnglishMatrix.AssemblyInfo.cs
+++ obj/Debug/net10.0/EnglishMatrix.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishMatrix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb34deca9ac1f9212f0c77b03f653dfc9627cf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishMatrix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishMatrix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
